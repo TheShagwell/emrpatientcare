5793316c64cb24430154a215380f504ea8091f1b
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-11-15 10:23:52 +0100

    Added StatCard component import and usage in Admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import { StatCard } from "@/components/StatCard";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -24,6 +25,27 @@ const Admin = () => {
           <p className="text-dark-700">
             Start the day with managing new appointments
           </p>
+        </section>
+
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={10}
+            label="Scheduled appointments"
+            icon={"/assets/icons/appointments.svg"}
+          />
+          <StatCard
+            type="pending"
+            count={8}
+            label="Pending appointments"
+            icon={"/assets/icons/pending.svg"}
+          />
+          <StatCard
+            type="cancelled"
+            count={32}
+            label="Cancelled appointments"
+            icon={"/assets/icons/cancelled.svg"}
+          />
         </section>
       </main>
     </div>
